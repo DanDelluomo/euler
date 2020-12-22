0e46bb9e6c118caaca004cd59c07d94cb2926925
Daniel Edward Dell'uomo <dan@MacBook-Pro.local>
2020-12-21 21:51:11 -0500

    marginally improved p. 46

--- 46.py
+++ 46.py
@@ -1,6 +1,7 @@
 import math
 primes = [2]
 twice_squares = []
+odd_composites = []
 
 def prime_test(num):
     for i in range(2, math.ceil(num ** 0.5) + 1):
@@ -8,20 +9,16 @@ def prime_test(num):
             return False
     return True
 
-for i in range(2, 100000):
+for i in range(3, 100000, 2):
     if prime_test(i):
         primes.append(i)
+    else:
+        odd_composites.append(i)
 
 for i in range(1, 1000000):
     i_sq = i * i
     twice_i_sq = 2 * i_sq
     twice_squares.append(twice_i_sq)
-
-odd_composites = []
-
-for i in range(3, 100000, 2):
-    if not prime_test(i):
-        odd_composites.append(i)
 
 set_twice_squares = set(twice_squares)
 
@@ -36,11 +33,3 @@ for odd_comp in odd_composites:
     if test == False:
         print(f"The answer is {odd_comp}")
         break
-
-
-
-
-
-
-
-
