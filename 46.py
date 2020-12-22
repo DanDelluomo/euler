980a4a4b820a6916c30a92f3d79fa1b0381c3392
Daniel Edward Dell'uomo <dan@MacBook-Pro.local>
2020-12-21 21:46:16 -0500

    Problem 46

---
+++ 46.py
@@ -0,0 +1,46 @@
+import math
+primes = [2]
+twice_squares = []
+
+def prime_test(num):
+    for i in range(2, math.ceil(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+for i in range(2, 100000):
+    if prime_test(i):
+        primes.append(i)
+
+for i in range(1, 1000000):
+    i_sq = i * i
+    twice_i_sq = 2 * i_sq
+    twice_squares.append(twice_i_sq)
+
+odd_composites = []
+
+for i in range(3, 100000, 2):
+    if not prime_test(i):
+        odd_composites.append(i)
+
+set_twice_squares = set(twice_squares)
+
+for odd_comp in odd_composites:
+    test = False
+    for prime in primes:
+        if prime >= odd_comp:
+            break
+        diff = odd_comp - prime
+        if diff in set_twice_squares:
+            test = True
+    if test == False:
+        print(f"The answer is {odd_comp}")
+        break
+
+
+
+
+
+
+
+
